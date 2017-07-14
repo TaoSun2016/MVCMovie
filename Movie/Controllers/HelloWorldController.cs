5c25e3175c425cf39ccdeb7f845bb71877d9da38
TaoSun2016 <kevinsun9516@gmail.com>
2017-07-15 10:09:53 +1200

    add controller and view Movie

---
+++ Movie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HellowWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello, "+name+"! Your ID is "+id);
+        }
+    }
+}
